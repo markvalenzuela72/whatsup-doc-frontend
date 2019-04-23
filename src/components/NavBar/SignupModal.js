9ed036bc18663094a0b21a9e04b5e0af7fd1a044
markvalenzuela72 <markvalenzuela72@gmail.com>
2019-04-23 18:00:11 +0400

    register

--- src/components/NavBar/SignupModal.js
+++ src/components/NavBar/SignupModal.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import 'materialize-css';
-import { Modal} from 'react-materialize';
+import { Modal, TextInput, Button } from 'react-materialize';
+import { registerRequest } from '../../helpers/network';
+import { saveUser } from '../../helpers/authentication';
 
-const SignupModal = () => {
-    const signupTrigger = <a href="#modal1" className="waves-effect waves-light btn white hoverable">Sign Up</a>
-    return (
-        <Modal header="Modal Header" trigger={signupTrigger}>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </p>
-    </Modal>
-    )
+class SignupModal extends Component {
+    state = {
+        error: null,
+        loggedin: null,
+        isregister: null
+      }
+      updateVal = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+      }
+      submitForm = async(e) => {
+        e.preventDefault();
+        this.setState({
+          error: null
+        })
+        try {
+          let response = await registerRequest({firstname: this.state.first_name, lastname: this.state.last_name, email: this.state.email, password: this.state.password});
+          saveUser(response);
+          this.setState({
+            loggedin: true,
+            isregister: true
+          })
+        }catch (e){
+          this.setState({
+            error: e.email
+          })
+        }
+      }
+    render() {
+        const signupTrigger = <a href="#modal1" className="waves-effect waves-light btn white hoverable">Create Account</a>
+        return (
+            <Modal className="signup-modal" fixedFooter header="Sign Up" trigger={signupTrigger}>
+            {this.state.isregister ? <Redirect to="/success/"/> : null}
+                <div className="row">
+                    <form onSubmit={this.submitForm} className="col s12">
+                        <div className="row">
+                            <TextInput name="first_name" s={12} type="text" onChange={this.updateVal} label="First Name" />
+                            <TextInput name="last_name" s={12} onChange={this.updateVal} label="Last Name" />
+                            <TextInput name="email" s={12} onChange={this.updateVal} label="Email" email validate />
+                            <TextInput name="password" s={12} onChange={this.updateVal} password label="Password" />
+                            <TextInput name="Confirmpassword" s={12} id="confirmPassword" onChange={this.updateVal} password label="Confirm Password" />
+                            <Button type="submit" waves="light">
+                                Register
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            </Modal>
+        )
+    }
 }
-export {SignupModal}
+export default SignupModal
---
+++ src/helpers/network.js
@@ -0,0 +1,71 @@
+import { getToken } from "./authentication";
+
+function loginRequest({email, password}){
+  return new Promise((resolve, reject) => {
+    fetch("http://localhost:5000/users/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    }).then(response => {
+      response.json().then(json => {
+        if (!response.ok){
+          return reject(json);
+        }
+        resolve(json);
+      }).catch(e => reject(e));
+    }).catch(e => reject(e))
+  });
+}
+
+function registerRequest({firstname, lastname, email, password}){
+  return new Promise((resolve, reject) => {
+    fetch("http://localhost:5000/users/register", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify({
+        firstname,
+        lastname,
+        email,
+        password
+      })
+    }).then(response => {
+      response.json().then(json => {
+        if (!response.ok){
+          return reject(json);
+        }
+        resolve(json);
+      }).catch(e => reject(e));
+    }).catch(e => reject(e))
+  });
+}
+
+function getDashboard(){
+  return new Promise((resolve, reject) => {
+    fetch("http://localhost:5000/dashboard", {
+      headers: {
+       'Authorization': getToken() 
+      }
+    }).then(response => {
+      if (!response.ok){
+        reject(new Error("Unathorized"));
+        return;
+      }
+      response.json().then(json => {
+        resolve(json.data);
+      }).catch(e => reject(e));
+    }).catch(err => reject(err))
+  })
+}
+
+export {
+  loginRequest,
+  registerRequest,
+  getDashboard
+}
